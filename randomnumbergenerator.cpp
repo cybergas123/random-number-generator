ff8c33021d86d2cfdc1d35a9995145115f580162
cybergas123 <103848773+cybergas123@users.noreply.github.com>
2023-11-27 16:50:52 +0100

    Add files via upload

---
+++ randomnumbergenerator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    //message
+    cout << "please input amount of numbers to generate : ";
+
+    //generatenumbers
+    int generatenumbers = 0;
+
+    cin >> generatenumbers;
+
+    //range of numbers from to 
+    int from = 0,to = 0;
+    cout << "please input range of numbers : ";
+    cout << "from : ";
+    cin >> from;    
+    cout << "to : ";
+    cin >> to;
+
+    srand(time(NULL));
+
+    for (int i = 0; i < generatenumbers; i++)
+    {
+        //num
+        int num = rand()%(to-from)+from;
+        cout << num << " ";
+    }
+}
